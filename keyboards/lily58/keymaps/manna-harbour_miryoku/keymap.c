38a8b9e9acb447cd4d274b91ae60e00970e6ae6c
cjohnson714 <cjohnson714@gmail.com>
2023-06-03 07:04:24 -0400

    Update keymap.c

--- keyboards/lily58/keymaps/manna-harbour_miryoku/keymap.c
+++ keyboards/lily58/keymaps/manna-harbour_miryoku/keymap.c
@@ -19,7 +19,7 @@ bool oled_task_user(void) {
     // Host Keyboard Layer Status
     oled_write_P(PSTR("Layer: "), false);
 
-    switch (get_highest_layer(layer_state)) {
+    switch (get_highest_layer(layer_state|default_layer_state)) {
       case BASE:
         oled_write_ln_P(PSTR("QWERTY"), false);
         break;
